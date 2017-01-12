78c2390e1876ed69ad3b315387fe3cad6024cd14
Barbara <barbara.pereiramendes@wfp.org>
2017-01-12 16:06:51 +0000

    trying js scroll trigger

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,1 @@
 var $ = require('../node_modules/jquery/dist/jquery.min.js');
-var ScrollMagic = require('../node_modules/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js');
-var addIndicators = require('../node_modules/scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js');
