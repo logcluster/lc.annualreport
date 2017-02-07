07981ab94d5f345a2ad1e554ac73cf4c9183b984
Barbara <barbara.pereiramendes@wfp.org>
2017-02-07 12:14:56 +0000

    photos and navigation

--- dist/bundle.js
+++ dist/bundle.js
@@ -18,7 +18,18 @@ var $ = require('../node_modules/jquery/dist/jquery.min.js');
   };
 }(jQuery, window));
 
-
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
 
 $("h1").inViewport(function(px){
   if(px) $(this).addClass("fade-down") ;
