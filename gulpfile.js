fe1150633d4d08df24311b9bedaa44926f69ebf4
André Lamelas <andre@lamelas.org>
2021-06-30 19:58:31 +0100

    Automation steps

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require("gulp"),
-    sass = require('gulp-sass'),
+    sass = require('gulp-dart-sass'),
     cleanCSS = require('gulp-clean-css'),
     ejs = require("gulp-ejs"),
     watch = require("gulp-watch"),
@@ -7,47 +7,49 @@ var gulp = require("gulp"),
     autoprefixer = require('autoprefixer'),
     browserSync = require('browser-sync').create();
 
-    // browsersync sass and templates change
-    gulp.task('serve', function() {
+var sections = require('./assets/sections.json');
 
-        browserSync.init({
-          server: "dist/"
-        });
+// browsersync sass and templates change
+gulp.task('serve', function() {
+  
+  browserSync.init({
+    server: "dist/"
+  });
+  
+  gulp.watch('assets/scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('assets/templates/**/*.ejs', gulp.series('templates'));
+  gulp.watch('dist/css/styles.css').on('change', browserSync.reload);
+  gulp.watch('dist/*.html').on('change', browserSync.reload);
+});
 
-        gulp.watch('assets/scss/**/*.scss', gulp.series('sass'));
-        gulp.watch('assets/templates/**/*.ejs', gulp.series('templates'));
-        gulp.watch('dist/css/styles.css').on('change', browserSync.reload);
-        gulp.watch('dist/*.html').on('change', browserSync.reload);
-    });
+// sass compilation
+gulp.task('sass', function () {
+  return gulp.src('assets/scss/**/*.scss')
+  .pipe(sass())
+  .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions', 'IE 11'] }) ]))
+  .pipe(gulp.dest('./dist/css'))
+  .pipe(browserSync.stream());
+});
 
-    // sass compilation
-    gulp.task('sass', function () {
-       return gulp.src('assets/scss/**/*.scss')
-          .pipe(sass())
-          .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions', 'IE 11'] }) ]))
-          .pipe(gulp.dest('./dist/css'))
-          .pipe(browserSync.stream());
-    });
+// minify css
+gulp.task('minify-css', function() {
+  return gulp.src('dist/css/*.css')
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(gulp.dest('dist/css/'));
+});
 
-    // minify css
-    gulp.task('minify-css', function() {
-      return gulp.src('dist/css/*.css')
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('dist/css/'));
-    });
+// compile ejs templates
+gulp.task('templates', function () {
+  return gulp.src('assets/templates/*.ejs')
+  .pipe(ejs({ "sections": sections.sections }, { ext:'.html' }))
+  .pipe(gulp.dest('dist'))
+  .pipe(browserSync.stream());
+});
 
-    // compile ejs templates
-    gulp.task('templates', function () {
-       return gulp.src('assets/templates/*.ejs')
-         .pipe(ejs({}, {ext:'.html'}))
-         .pipe(gulp.dest('./dist'))
-         .pipe(browserSync.stream());
-    });
+// watch sass and templates
+gulp.task('watch', function() {
+  gulp.watch(['assets/scss/**/*.scss'] , ['sass']);
+  gulp.watch('assets/templates/*.ejs' , ['templates']);
+});
 
-    // watch sass and templates
-    gulp.task('watch', function() {
-        gulp.watch(['assets/scss/**/*.scss'] , ['sass']);
-        gulp.watch('assets/templates/*.ejs' , ['templates']);
-    });
-
-    gulp.task('default', gulp.series('serve'));
+gulp.task('default', gulp.series('serve'));
